a42169be86c507b7a0c339d4ea7f13dc2d88e4f5
Eric Van Wyk <evw@cs.umn.edu>
2016-09-20 12:05:00 -0500

    adding MDA and MDWA to tests

--- test/the_tests.sh
+++ test/the_tests.sh
@@ -5,14 +5,42 @@ set -e
 # turn on verbose option, which echos commands to stdout
 set -v
 
-cd ../artifact
+# cd to top of extension directory.
+cd ../
+
+#
+# Build: building the artifact
+#
+cd artifact
+pwd
 ./build.sh --clean
+cd ../
+
+#
+# MDA: run modular determinsim analysis
+#
+cd modular_analyses/determinism
+pwd
+./run.sh --clean
+cd ../../
+
+#
+# MWDA: run modular well-definedess analysis
+#
+cd modular_analyses/well_definedness
+pwd
+./run.sh --clean
+cd ../../
 
 # this currently 'works' but reports errors and thus fails
-cd ../examples
+cd examples
+pwd
 ./compile.sh skeleton.xc
 
 ./a.out
+
+cd ../
+pwd
 
 set +v
 
